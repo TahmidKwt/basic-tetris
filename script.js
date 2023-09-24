d33cf66a903762393501a664addd6df0f0599c09
Coffin <100180424+TahmidKwt@users.noreply.github.com>
2023-09-25 03:05:37 +0600

    Add function to create grid of boxes and set initial size to 10×20

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const container = document.querySelector(".container");
 
+const displayGrid = (columns, rows) => {
+  
+  for (let i = 0; i < columns; i++) {  
+    let column = document.createElement("div");
+    column.classList.add("column");
+    
+    for (let j = 0; j < rows; j++) {  
+      let row = document.createElement("div");
+      row.classList.add("row");
+      column.insertBefore(row, lastRow);
+    } 
+    container.appendChild(column);
+  }
+  
+};
+
+displayGrid(10,20);
