52beb13d360b7f8e22c8996bdccc5c700ef5d216
Coffin <100180424+TahmidKwt@users.noreply.github.com>
2023-09-25 03:16:04 +0600

    Create shapes and variations of each tetrominos

--- script.js
+++ script.js
@@ -19,4 +19,51 @@ const displayGrid = (columns, rows) => {
 displayGrid(10,20);
 
 let boxes = Array.from(document.querySelectorAll(".container .row"));
+const width = 10;
+
+const lTetromino = [  
+    
+  [0, 1, 2, width*2+1],  
+  [0, width*2+1, width*4+2, width*4+3], 
+  [2, width*2+1, width*2+2, width*2+3],  
+  [0, 1, width*2+1, width*4+2]
+   
+]
+
+const zTetromino = [
+    
+  [1, width*2+2, width*2+1, width*4+2],  
+  [0, 1, width*2+2, width*2+3],  
+  [1, width*2+2, width*2+1, width*4+2],  
+  [0, 1, width*2+2, width*2+3]
+
+]
+
+const tTetromino = [
+ 
+  [1, width*2+1, width*2+2, width*4+3],   
+  [width*2+1, width*2+2, width*2+3, width*4+3],    
+  [0, width*2+1, width*2+2, width*4+2],
+  [1, width*2+1, width*2+2, width*2+3]
+  
+]
+
+const oTetromino = [
+
+  [0, 1,width*2+1, width*2+2], 
+  [0, 1, width*2+1, width*2+2],   
+  [0, 1, width*2+1, width*2+2],   
+  [0, 1, width*2+1, width*2+2]
+ 
+]
+
+const iTetromino = [
+  
+  [width*2+1, width*2+2, width*2+3, width*2+4],
+  [0, width*2+1, width*4+2, width*6+3],   
+  [width*2+1, width*2+2, width*2+3, width*2+4],
+  [0, width*2+1, width*4+2, width*6+3]
+  
+]
+   
 
